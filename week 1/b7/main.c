a2dde7742c7ca95ba952e80d8c9ded4efc108d05
Remco <Remco.boys@gmail.com>
2016-02-15 18:23:25 +0100

    assignment coding b7a

--- week 1/b7/main.c
+++ week 1/b7/main.c
@@ -28,23 +28,10 @@ void s3(void);
 void start(void);
 void end(void);
 
-typedef enum{D7,D6,D5}BEvent;
-typedef enum{START_state,STATE_1,STATE_2,STATE_3,END_State}BOARD_STATE;	
+typedef enum{D7,D6,D5,NoEvent}BEvent;	//button events, indicates which button you have pressed, or not even 1
+typedef enum{START_state,STATE_1,STATE_2,STATE_3,END_State}BOARD_STATE;	//state of the board
 
-
-typedef struct
-{
-	void (*init)(void);
-	void (*body)(void);
-	void (*exit)(void);
-}State_Struct;
-
-State_Struct Start =	{NULL,start,NULL};
-State_Struct State1 =	{NULL,s1,	NULL};
-State_Struct State2 =	{NULL,s2,	NULL};
-State_Struct State3 =	{NULL,s3,	NULL};
-State_Struct End =		{NULL,end,	NULL};
-
+//shows in which states the board is
 void s1(void)
 {
 	PORTD = 0b00000010;
@@ -70,84 +57,166 @@ void end(void)
 	PORTD = 0b00010000;
 }
 
-BOARD_STATE state = START_state;
+BEvent bevent = NoEvent;		//last given event
+BOARD_STATE state = START_state;//current state
+int newEvent = 0;	//shows there has been a new trigger: 1 = true, 0 = false
 
-void handleEvent(BEvent bevent)
+
+//sets the last button event
+BEvent getButton()
 {
+	if((PIND & 0b10000000))
+	{
+		if(bevent != D7)
+		{
+			newEvent = 1;		//new event had triggerd
+			return D7;
+		}		
+		else
+		{
+			newEvent = 0;
+		}
+	}
+	else if((PIND & 0b01000000))
+	{
+		if(bevent != D6)
+		{
+			newEvent = 1;
+			return D6;
+		}	
+		else
+		{
+			newEvent = 0;
+		}	
+	}
+	else if((PIND & 0b00100000))
+	{
+		if(bevent != D5)
+		{
+			newEvent = 1;
+			return D5;
+		}	
+		else
+		{
+			newEvent = 0;
+		}	
+	}
+	else
+	{
+		newEvent = 0;
+		return NoEvent;
+	}	
+}
+
+void handleState()
+{
+	if(newEvent == 0)		//if there was no new event, the state does not have to update
+	{
+		return;
+	}
+
 	switch(state)
 	{
 	case START_state:
 		switch(bevent)
 		{
 			case D5:
-			closeState(START_state);
-			state = STATE_1;			
+			state = STATE_2;
+			s2();
 			break;
 			case D6:
-			closeState(START_state);
-			state = STATE_2;
+			state = STATE_1;
+			s1();
 			break;
 			case D7:
 			break;
+			case NoEvent:
+			break;
 		}
 	break;
+
 	case STATE_1:
 	switch(bevent)
 	{
 		case D5:
-		closeState(STATE_1);
 		state = STATE_2;
+		s2();
 		break;
-		case D6:		
+		case D6:
 		break;
 		case D7:
-		closeState(STATE_1);
 		state = START_state;
+		start();
+		break;
+		case NoEvent:
 		break;
 	}
 	break;
-	}
-	handleState(state);
-}
 
-void closeState(State_Struct state)
-{
-	if(state.exit != NULL)
-	{
-		state.exit();
-	}
-}
+	case STATE_2:
+		switch(bevent)
+		{
+			case D5:
+			state = STATE_3;
+			s3();
+			break;
+			case D6:
+			state = STATE_1;
+			s1();
+			break;
+			case D7:
+			state = START_state;
+			start();
+			break;
+			case NoEvent:
+			break;
+		}
+	break;
 
-void handleState(State_Struct state)
-{
-	if(state.init != NULL)
-	{
-		state.init();
-	}
-	if(state.body != NULL)
-	{
-		state.body();
+	case STATE_3:
+		switch(bevent)
+		{
+			case D5:			
+			case D6:
+			state = END_State;
+			end();
+			break;
+			case D7:
+			state = START_state;
+			start();
+			break;
+			case NoEvent:
+		break;
+		}
+		break;
+
+	case END_State:
+		switch(bevent)
+		{
+			case D5:
+			break;
+			case D6:
+			break;
+			case D7:
+			state = START_state;
+			start();
+			break;
+			case NoEvent:
+			break;
+		}
+	break;
 	}
 }
 
 int main(void)
 {
-	DDRD = 0b00011111;
-
-	start();	
+	DDRD = 0b00011111;	// Pins PORTD<7:5> input, PORTD<4:0> output
+	start();	//installation function
 
 	while(1)
-	{
-		//start();
-		//wait(500);
-		//s1();
-		//wait(500);
-		//s2();
-		//wait(500);
-		//s3();
-		//wait(500);
-		//end();
-		//wait(500);
+	{		
+		bevent = getButton();
+		handleState();		
 	}
 
 	return 1;
